071871888794d2e4f0be11d2963d6b81d8e12458
MirrorkaYT <letsplayobzor@gmail.com>
2015-08-05 12:05:58 +0300

    improved page-proofs and added validates

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,8 @@
 class Article < ActiveRecord::Base
 	belongs_to :user
+  validates :priority, presence: true, length: { maximum:3 }
+  validates :priority, presence: true, numericality: { greater_than_or_equal_to: 1, message: "%{count} is less than 0" }
+  validates :title, presence: true, length: {within: 5..25}
+  validates :text, presence: true, length: { maximum: 1000 }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
 	has_many :articles
 	has_secure_password
-	validates_uniqueness_of :email, :nickname
+
+	EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :nickname, presence: true,
+            length: {maximum: 20}
+  validates :email, presence: true,
+            format: {with: EMAIL_REGEX}
+  validates :password,
+            length: {within: 6..40}, on: :create
+  validates_uniqueness_of :email, :nickname
 end
